bfacc3b568f3fc14278a0ea22cbf5302274a9494
Felipe <felipe.miranda@penseavanti.com.br>
2021-11-19 16:08:05 -0300

    Page Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import {Link} from 'react-router-dom'
+import api from '../../services/api'
 import styles from './Home.module.css'
 
 const Home = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(()=>{
+   async function loadFilmes(){ 
+     const response = await api.get('r-api/?api=filmes')
+    setMovies(response.data)
+   
+  }
+  loadFilmes()
+},[])
+
+
   return (
     <div className={styles.container}>
-      Home
+      {movies.map(({id,nome,foto})=> {
+        return(
+        <div className={styles.card} key={id}>
+          <p className={styles.titleCard}>{nome}</p>
+          <img className={styles.imageCard} src={foto} alt={nome} />
+          <Link to="/" className={styles.buttonCard}>Acessar</Link>
+        </div>
+      )})}
+
     </div>
   )
 }
